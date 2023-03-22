399e134041d23356d0100bc0a18e31b71a05f1bd
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2023-03-22 15:37:32 -0600

    Tail winds

--- main_project/occ/src/App.vue
+++ main_project/occ/src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-// import Greet from "./components/Greet.vue";
+  // This starter template is using Vue 3 <script setup> SFCs
+  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+  // import Greet from "./components/Greet.vue";
+
+  // 
+  import TopBar from "./components/TopBar.vue";
+
 </script>
 
 <template>
-  <div class="text-red-500">
-    TEXT HERE ?///
-
-  </div>
+  <TopBar/>
 
       <!-- <Greet /> -->
 </template>
---
+++ main_project/occ/src/components/TopBar.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  // const greetMsg = ref("");
+  // const name = ref("");
+
+
+    // Icons
+    import Magnify from 'vue-material-design-icons/Magnify.vue';
+    import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
+    import VideoBox from 'vue-material-design-icons/VideoBox.vue';
+    import AccountGroup from 'vue-material-design-icons/AccountGroup.vue';
+    import AccountCash from 'vue-material-design-icons/AccountCash.vue';
+    import AccountCog from 'vue-material-design-icons/AccountCog.vue';
+    import ExitToApp from 'vue-material-design-icons/ExitToApp.vue';
+    import ApplicationBrackets from 'vue-material-design-icons/ApplicationBrackets.vue';
+
+
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  //   greetMsg.value = await invoke("greet", { name: name.value });
+  // }
+</script>
+
+<template>
+  <div class="text-red-500">
+    TEXT HERE
+  </div>
+</template>
