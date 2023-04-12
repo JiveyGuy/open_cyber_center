a68bdedd76525780297da48a9c223da2152e2651
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2023-04-12 14:24:21 -0600

    Added launch game

---
+++ prototypes/launch_game/src/main.rs
@@ -0,0 +1,20 @@
+use std::process::Command;
+struct Program{
+    location: String, // where the .exe is 
+}
+
+async fn launch_program(prog: Program) {
+    let _output = Command::new(prog.location)
+        .spawn()
+        .expect("failed to execute process");
+}
+
+fn main() {
+    let gmod_program = Program{
+        location: "G:\\SteamLibrary\\steamapps\\common\\Among Us.exe".to_string(),
+    };
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(launch_program(gmod_program));
+
+    println!("done.");
+}
