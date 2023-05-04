67f0d273e6109edd03ea5c9f748f003c89e0d596
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2023-05-03 20:19:40 -0600

    fixed loading anim

--- prototypes/loading_anim/src/App.vue
+++ prototypes/loading_anim/src/App.vue
@@ -1,50 +1,33 @@
+<script setup lang="ts">
+    import { ref } from "vue";
+    // import VideoBackground from 'vue-responsive-video-background-player'
+    import login_video from './assets/login_video.mp4';
+</script>
+
 <template>
-    <div class="w-screen h-screen bg-black">
-      <div class="flex justify-center items-center h-full">
-        <div class="relative">
-          <div class="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-            <div class="flex flex-col">
-              <div class="text-white text-lg mb-2">S Tier</div>
-              <div ref="sTier" class="flex flex-wrap">
-                <div v-for="(image, index) in sTierImages" :key="index" class="w-24 h-24 mr-2 mb-2">
-                  <img :src="image" class="w-full h-full object-cover rounded-lg shadow-md">
+    <div class="bg-cover w-screen h-screen bg-black">
+        
+        <div class="px-10 py-10">
+            <div class="grid gap-8 items-start justify-center">
+                <div class="relative group">
+
+                    <div class="animate-pulse absolute -inset-0.5 bg-gradient-to-r from-green-700 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"/>
+                    
+                    <div class="relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
+                        <span class="flex items-center space-x-5">                  
+                            <span class="font-old items-center text-gray-100">
+                                Loading
+                            </span>
+                        </span>
+                    </div>
+
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
+        <div class="grid gap-8 items-start justify-center">
+            <img class=" self-center" src="./assets/load.gif" alt="Loading" />
+        </div>
+        
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import interact from 'vue-interactjs';
-  
-  const sTierImages = [
-    'https://picsum.photos/id/237/200/300',
-    'https://picsum.photos/id/238/200/300',
-    'https://picsum.photos/id/239/200/300',
-    'https://picsum.photos/id/240/200/300',
-  ];
-  
-  interact('.draggable').draggable({
-    modifiers: [
-      interact.modifiers.restrictRect({
-        endOnly: true,
-        elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
-      }),
-    ],
-    listeners: {
-      move(event) {
-        const target = event.target;
-        const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-        const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-        target.style.webkitTransform = target.style.transform = `translate(${x}px, ${y}px)`;
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-      },
-    },
-  });
-  
-  </script>
-  
+</template>
+
