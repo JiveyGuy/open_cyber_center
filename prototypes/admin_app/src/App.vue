eac4766cf074be2c5d0decfa0b00f732cc6d1e4f
cmuel10 <cmuel10@nmsu.edu>
2023-05-05 04:16:38 -0600

    changes to admin_app / admin_app_rust to account for JSON

--- prototypes/admin_app/src/App.vue
+++ prototypes/admin_app/src/App.vue
@@ -1,44 +1,26 @@
-<!-- src/components/PeopleList.vue -->
-
 <template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">People List</h1>
-    <ul>
-      <li v-for="(person, index) in people" :key="index">
-        <input
-          type="text"
-          v-model="person.name"
-          class="border border-gray-400 rounded-md p-2 mr-2"
-        />
-        <input
-          type="text"
-          v-model="person.address"
-          class="border border-gray-400 rounded-md p-2"
-        />
-      </li>
-    </ul>
+  <div class="container mx-auto">
+    <h1 class="text-4xl mb-8">Game List</h1>
+    <div class="grid grid-cols-2 gap-6">
+      <div
+        v-for="game in games"
+        :key="game.id"
+        class="bg-white p-6 rounded shadow"
+      >
+        <img :src="game.img_url" :alt="game.name" class="w-full h-48 object-cover mb-4 rounded" />
+        <h2 class="text-2xl mb-4">{{ game.name }}</h2>
+        <p class="mb-4">{{ game.description }}</p>
+        <input v-model="game.year" type="text" class="mb-4" />
+        <input v-model="game.rating" type="text" class="mb-4" />
+        <input v-model="game.video_url" type="text" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref } from 'vue';
+import gamesData from './games.json';
 
-const people = [
-  { name: "Alice", address: "123 Main St" },
-  { name: "Bob", address: "456 Oak Ave" },
-  { name: "Charlie", address: "789 Elm Rd" },
-];
-
-export default defineComponent({
-  name: "PeopleList",
-  setup() {
-    return { people };
-  },
-});
+const games = ref(gamesData);
 </script>
-
-<style>
-li {
-  margin-bottom: 0.5rem;
-}
-</style>
--- prototypes/admin_app_rust/src/main.rs
+++ prototypes/admin_app_rust/src/main.rs
@@ -1,5 +1,110 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, vec};
+use bson::oid::ObjectId;
+use futures::{TryStreamExt};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use mongodb::{options::{ClientOptions, ResolverConfig}};
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GameStruct {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    name: String,
+    description: String,
+    year: String,
+    rating: String,
+    video_url: String,
+    img_url: String,
+}
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Load the MongoDB connection string from an environment variable:
+    let client_uri =
+        env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+    // A Client is needed to connect to MongoDB:
+    // An extra line of code to work around a DNS issue on Windows:
+    let options =
+        ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare()).await.expect("error");
+    let client = mongodb::Client::with_options(options).unwrap();
+
+    // Print the databases in our MongoDB cluster:
+    println!("Databases:");
+    for name in client.list_database_names(None, None).await.expect("error") {
+        println!("- {}", name);
+    }
+
+    // Get the 'movies' collection from the 'sample_mflix' database:
+    let db = client.database("test");
+    
+    println!("Collections:");
+    for name in db.list_collection_names(None).await.expect("error") {
+        println!("- {}", name);
+    }
+
+    let games = db.collection::<GameStruct>("GameList");
+
+
+    println!("Indexs:");
+    for name in games.list_index_names().await.expect("error") {
+        println!("- {}", name);
+    }
+
+    let cursor = games.find(None, None).await.expect("error");
+
+    print!("debug info:");
+    // println!("{}", cursor.);
+
+    let vec_cursor = cursor.try_collect().await.unwrap_or_else(|_| vec![]);
+    println!("len: {}", vec_cursor.len());
+
+
+    // for entry in vec_cursor {
+    //     println!("{}", entry.name);
+    // }
+    
+    //let vec_cursor = vec![1, 2, 3];
+    let mut buffer_string = String::new();
+    buffer_string.push('[');
+    
+    for (i, entry) in vec_cursor.iter().enumerate() {
+        buffer_string.push_str(&serde_json::to_string(&entry).unwrap());
+        if i < vec_cursor.len() - 1 {
+            buffer_string.push_str(", ");
+        }
+    }
+    
+    buffer_string.push(']');
+    println!("{}", buffer_string);
+
+    let mut file = File::create("output.json")?;
+    file.write_all(buffer_string.as_bytes())?;
+    Ok(())
+
+   
+
+
+
+    
+
+
+    
+    // instead of just the name - take name, id... etc and put into vector ^^^ above struc -- 
+    // using the tutorial jason sent in discord 
+    
+    // use two string example
+    // print out to make sure everything is working correctly 
+    // figure out how to save file in rust 
+
+
+}
+
+
+
 
     // read in all games from mongo
 
@@ -27,4 +132,4 @@ fn main() {
     // ]
 
     // reference code in test_functions in prototypes
-}
+
