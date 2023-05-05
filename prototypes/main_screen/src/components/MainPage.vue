10cf92523a48ae32bfd04ef45d0d23293e292ce1
cmuel10 <cmuel10@nmsu.edu>
2023-05-04 20:22:41 -0600

    clickable button attempt
    made changes to MainPage.vue to attempt to make buttons clickable

--- prototypes/main_screen/src/components/MainPage.vue
+++ prototypes/main_screen/src/components/MainPage.vue
@@ -4,11 +4,34 @@
   import TopBar from "./TopBar.vue";
   import Stats from "./Stats.vue";
   import GameCarousel from "./GameCarousel.vue"
+  import OtherPage from "./OtherPage.vue";
+
 
   let isLivePage = ref(false);
   let isTierPage = ref(false);
   let isPlayPage = ref(false);
   let isFundPage = ref(false);
+
+  let isHomePage = ref(false);
+  
+
+  function showLivePage() {
+    isLivePage.value = true;
+    isFundPage.value = false;
+    isHomePage.value = false;
+  }
+
+  function showFundPage() {
+    isFundPage.value = true;
+    isLivePage.value = false;
+    isHomePage.value = false;
+  }
+
+  function showHomePage() {
+    isHomePage.value = true;
+    isLivePage.value = false;
+    isFundPage.value = false;
+  }
 
 
 </script>
@@ -20,10 +43,10 @@
       <div class="grid grid-cols-12 gap-0 grid-rows-8 scrollbar scrollbar-thumb-amber-300 scrollbar-track-slate-600"  style="height: calc(100% - 64px); overflow: scroll;">
           
         <div class="col-start-1 row-span-6 row-start-1 col-span-2 m-4">
-          <TopBar @live="isLivePage = true"/>
+          <TopBar @live="showLivePage" 
+                  @fund="showFundPage" 
+                  @home="showHomePage"/>
         </div>
-        
-        <!-- <div v-if="!isLivePage"> -->
 
           <div class="items-center row-start-1 row-span-1 col-start-3 col-end-8 m-4">
             <Stats/>
@@ -35,6 +58,26 @@
 
           <div class="items-center row-start-2 row-span-1 col-start-3 col-end-13 m-4 text-amber-500">
             <GameCarousel/>
+          </div>
+
+          <!-- div section for HOME page -->
+          <div v-if="isHomePage && !isLivePage && !isFundPage">
+            <MainPage />
+          </div>
+
+          <!-- div section for FUND page -->
+          <div v-if="!isHomePage && !isLivePage && isFundPage">
+            <OtherPage />
+          </div>
+
+          <!-- div section for LIVE page -->  
+          <div v-if="!isHomePage && isLivePage && isFundPage">
+            <OtherPage />
+          </div>
+
+          <!-- div section for other pages when conditions are not met-->
+          <div v-else>
+            <OtherPage />
           </div>
 
         </div>
