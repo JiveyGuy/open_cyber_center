1ed8ad1992e3ffec9bed6c8c41d662ba04df3cbc
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2023-05-10 23:19:45 -0600

    Update main.rs
    beginning of the connection - trouble with async

--- prototypes/admin_app/src-tauri/src/main.rs
+++ prototypes/admin_app/src-tauri/src/main.rs
@@ -1,3 +1,22 @@
+
+use std::{env, vec};
+use bson::oid::ObjectId;
+// use futures::{TryStreamExt};
+use serde::{Deserialize, Serialize};
+use mongodb::{options::{ClientOptions, ResolverConfig}};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GameStruct {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    id: Option<ObjectId>,
+    name: String,
+    description: String,
+    year: String,
+    rating: String,
+    video_url: String,
+    img_url: String,
+}
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
@@ -12,6 +31,17 @@ fn update_entry(_id: i16, name: &str, description: &str, year: &str, rating: &st
     println!("I was called with game img_url: \"{}\"", img_url);
     println!("I was called with game exe_url: \"{}\"", exe_url);
     
+
+     // Load the MongoDB connection string from an environment variable:
+     let client_uri =
+     env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
+
+ // A Client is needed to connect to MongoDB:
+ // An extra line of code to work around a DNS issue on Windows:
+//  let options =
+//      ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare()).await.expect("error");
+//  let client = mongodb::Client::with_options(options).unwrap();
+
 
     // search mongo db with cursor for game name
     
