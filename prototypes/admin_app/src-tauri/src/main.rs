956c3b3b868c08f43bee94afd55e48900b6c48de
cmuel10 <cmuel10@nmsu.edu>
2023-05-12 05:41:30 -0600

    updated color scheme for admin_app

--- prototypes/admin_app/src-tauri/src/main.rs
+++ prototypes/admin_app/src-tauri/src/main.rs
@@ -1,5 +1,7 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// Prevents additional console window on Windows in release
+// !!! DO NOT REMOVE !!! 
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 
 use std::{env, vec};
 use bson::{doc, oid::ObjectId};
@@ -9,6 +11,7 @@ use serde_json;
 use mongodb::{options::{ClientOptions, ResolverConfig}};
 use std::fs::File;
 use std::io::prelude::*;
+
 
 #[derive(Serialize, Deserialize, Debug)]
 struct GameStruct 
@@ -22,6 +25,7 @@ struct GameStruct
     video_url: String,
     img_url: String,
 }
+
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FilterOnIdStruct 
@@ -52,58 +56,44 @@ async fn update_entry(id: &str, name: &str, description: &str, year: &str, ratin
 {
     // connect to mongo
     let db: mongodb::Database = get_mongo_handle().await;
-
     
-    // search mongo db with cursor for game ID
-    // find with pattern in mongo for matching id
-    //println!("User id = {}", id);
-
-     // Create a cursor to search for the game ID
-     let collection = db.collection::<GameStruct>("GameList");
-     //  let oid1 = ObjectId::from_str(id)?; //same as o in the 
-
-     let filter_struct = FilterOnIdStruct{ 
+    // Create a cursor to search for the game ID
+    let collection = db.collection::<GameStruct>("GameList");
+    
+    let filter_struct = FilterOnIdStruct { 
         _id: Some(ObjectId::parse_str(id).expect("Failed to parse as object ID!"))
-     };
-
-
+    };
+    
     //  println!("Object id = {}", ); //todo
-     let filter = doc! { "_id": filter_struct._id };
-     let update = doc! { "$set" : {
-            "name": name,
-            "description": description,
-            "year": year,
-            "rating": rating,
-            "video_url": video_url,
-            "img_url": img_url,
-            "exe_url": exe_url,
-        }
-      };
+    let filter = doc! { "_id": filter_struct._id };
 
-     match collection
-        .find_one_and_update(filter, update, None).await{
-            Ok(_res) => {
-                // println!("{}", res);
-                println!("Updated one");
-            },
-            Err(e) => {
-                println!("{}", e);
-                println!("Error");
-            }      
+    let update = doc! 
+    {
+        "$set" : {
+        "name": name,
+        "description": description,
+        "year": year,
+        "rating": rating,
+        "video_url": video_url,
+        "img_url": img_url,
+        "exe_url": exe_url,
         }
-   
+    };
+    
+    match collection
+    .find_one_and_update(filter, update, None).await {
+        Ok(_res) => {
+            println!("Updated one");
+        },
+        
+        Err(e) => {
+            println!("{}", e);
+            println!("Error");
+        }      
+    }
+    
     return Ok(format!("Ok"));
 }
-    
-    // when found, update all info 
-    //  Make sure it only matches 1 game 
-
-    // Change info to name: &str, description: &str, year: &str, rating: &str, video_url: &str, img_url: &str, exe_url: &str
-    
-    // push to mongo (using some db.function)
-
-    // make sure to sleep like 1 sec
-    // write_games_json("./games.json").await.expect("Failed.");
 
 
 async fn write_games_json(path_to_save:&str) -> std::io::Result<()> 
@@ -136,6 +126,7 @@ async fn write_games_json(path_to_save:&str) -> std::io::Result<()>
     
     Ok(())
 }
+
 
 #[tokio::main]
 async fn main() 
--- prototypes/admin_app/src/App.vue
+++ prototypes/admin_app/src/App.vue
@@ -1,73 +1,69 @@
 <template>
-  <div class="w-full h-full mx-auto bg-black border-gray-800 p-4 ">
+<div class="w-full h-full mx-auto bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 border-gray-800 p-4">
   <div class="flex text-white justify-center">
-    OPEN CYBER CENTER ADMIN EDITOR
+    <h1 class="text-white text-4xl font-bold" style="text-shadow: 2px 2px 0 #6B46C1, -2px -2px 0 #6B46C1;">
+      OPEN CYBER CENTER ADMIN EDITOR
+    </h1>
   </div>
+  
+  <h2 class="text-white text-3xl font-semibold underline mt-7 mr-4 mb-5">Game List</h2>
+  
+  <div class="grid grid-cols-1 md:grid-cols-2 lg: grid-cols-3 gap-6">
+    <div
+    v-for="game in games"
+    :key="game._id.$oid"
+    class="rounded shadow bg-violet-400 backdrop-filter backdrop-blur-sm"
+    >
     
-    <h1 class="text-4xl mb-8">Game List</h1>
+    <h2 class="text-2xl mb-5 text-gray-800 font-bold">{{ game.name }}</h2>
+    <p class="mb-5 ">{{ game.description }}</p>
+    
+    <!-- add for every item in .json  -->
+    <div class=" grid grid-cols-3"> 
+      <div class = " row-span-6">
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg: grid-cols-3 gap-6">
-      <div
-        v-for="game in games"
-        :key="game._id.$oid"
-        class="rounded shadow bg-slate-500 "
-      >
-      
-        <h2 class="text-2xl mb-4">{{ game.name }}</h2>
-        <p class="mb-4">{{ game.description }}</p>
-        <!-- <input v-model="game.year" type="text" class="mb-4" /> -->
-        <!-- input v-model="game.rating" type="text" class="mb-4" /> -->
-        
-        <!-- add for every item in .json  -->
-        <div class=" grid grid-cols-2"> 
-        <div class = " row-span-6">
-          <div class = "text-white text-slate-900">Name:
-            <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.name" type="text" />
-          </div>
-
-          <div class = "text-white text-slate-900">Description:
-            <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.description" type="text" />
-          </div>
-
-          <div class = "text-white text-slate-900">Year:
-            <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.year" type="text" />
-          </div>
-
-          <div class = "text-white text-slate-900">Rating:
-            <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.rating" type="text" />
-          </div>
-
-          <div class = "text-white text-slate-900">Video Link:
-            <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.video_url" type="text" />
-          </div>
-
-          <div class = "text-white text-slate-900">Image Link:
-            <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.img_url" type="text" />
-          </div>
-
-          <div class = "text-white text-slate-900">Exe Link:
-            <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.exe_url" type="text" />
-          </div>
-
-          <div class=" rounded-lg h-fit w-fit justify-end bg-green-800 px-1 py-1 transition-all duration-200 transform hover:scale-105 " type="button" @click="update_entry(game._id.$oid, game.name, game.description, game.year, game.rating, game.video_url, game.img_url, game.exe_url)">
-            <button class="text-white" >UPDATE</button>
-          </div>
-
-        
-          <div class = "col-start-3 h-fit row-span-4">
-            <img :src="game.img_url" :alt="game.name" class="w-full h-full object-cover mb-4 rounded" />
-          </div>
-        </div>
-      </div>
+        <div class = "text-white text-slate-900 mb-4">Name:
+          <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.name" type="text" />
         </div>
         
+        <div class = "text-white text-slate-900 mb-4">Description:
+          <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.description" type="text" />
+        </div>
         
+        <div class = "text-white text-slate-900 mb-4">Year:
+          <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.year" type="text" />
+        </div>
         
+        <div class = "text-white text-slate-900 mb-4">Rating:
+          <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.rating" type="text" />
+        </div>
+        
+        <div class = "text-white text-slate-900 mb-4">Video Link:
+          <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.video_url" type="text" />
+        </div>
+        
+        <div class = "text-white text-slate-900 mb-4">Image Link:
+          <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.img_url" type="text" />
+        </div>
+        
+        <div class = "text-white text-slate-900 mb-4">Exe Link:
+          <input class = "py-2 rounded-sm text-white bg-neutral-700" v-model="game.exe_url" type="text" />
+        </div>
+        
+        <div class="button-container">
+          <button class="rounded-lg bg-gray-800 px-1 py-1 transition-all duration-200 transform hover:scale-105 text-white" type="button" @click="update_entry(game._id.$oid, game.name, game.description, game.year, game.rating, game.video_url, game.img_url, game.exe_url)">UPDATE</button>
+          <button class="rounded-lg bg-gray-800 px-1 mb-4 mt-4 py-1 transition-all duration-200 transform hover:scale-105 text-white" type="button" @click="add_entry()">PLUS</button>
+        </div>
+
+        <div class = "col-start-3 h-fit row-span-4">
+          <img :src="game.img_url" :alt="game.name" class="w-full h-full object-cover mb-4 rounded" />
+        </div>
       </div>
     </div>
+  </div>
 
-
-  
+  </div>
+</div>
 </template>
 
 <script setup lang="ts">
